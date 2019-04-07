de3aef09e52b217208660d09a00fb26772ccb1af
Hugo Sartori <hugo_sart@hotmail.com>
2019-04-07 02:45:58 -0300

    Initial commit

---
+++ src/main/java/com/hugovs/gls/util/DateUtils.java
@@ -0,0 +1,30 @@
+package com.hugovs.gls.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Date utilities.
+ *
+ * @author Hugo Sartori
+ */
+public class DateUtils {
+
+    private DateUtils() {
+        //no instance
+    }
+
+    /**
+     * The default data format definition of this application.
+     */
+    public static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+
+    /**
+     * Get the current system timestamp.
+     * @return the current system timestamp.
+     */
+    public static long timestamp() {
+        return new Date().getTime();
+    }
+
+}
